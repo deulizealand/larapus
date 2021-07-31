01d7bca239a80c52637587c3eeb6c3364cd07937
User <andikajayawiguna@gmail.com>
2021-08-01 00:27:49 +0800

    confirm before delete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,6 +90,7 @@
     <!-- <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script> -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
     @yield('scripts')
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/all.min.js') }}"></script>
