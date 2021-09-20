5b1c458be2587dc37987b4c53db7994ac6727cba
andikajayawiguna <andikajayawiguna@gmail.com>
2021-09-20 23:31:03 +0800

    create view dashboard for admin and member

---
+++ resources/views/dashboard/admin.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="panel-title">Dashboard</h2>
+                </div>
+                <div class="card-body">
+                    Selamat datang di Menu Administrasi Larapus. Silahkan pilih menu administrasi yang diinginkan.
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
