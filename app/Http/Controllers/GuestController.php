f905fe4e39b6b16a8c4cafc7a3150ba70b55f715
andikajayawiguna <andikajayawiguna@gmail.com>
2021-09-20 22:22:52 +0800

    create get list book for peminjam buku @index

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use Yajra\DataTables\Html\Builder;
+use App\Book;
+use Laratrust\LaratrustFacade as Laratrust;
 
 class GuestController extends Controller
 {
-    
+    public function index(Request $request, Builder $htmlBuilder)
+    {
+        if($request->ajax()) {
+            $books = Book::with('author');
+            return DataTables::of($books)->addColumn('action', function($book){
+                if(Laratrust::hasRole('admin')) return '';
+                return '<a class="btn btn-xs btn-primary" href="#">Pinjam</a>';
+            })->make(true);
+        }
+        $html = $htmlBuilder
+        ->addColumn(['data' => 'title', 'name' => 'title', 'title' => 'Judul'])
+        ->addColumn(['data' => 'author.name', 'name' => 'author.name', 'title' => 'Penulis'])
+        ->addColumn(['data' => 'action', 'name' => 'action', 'title' => '', 'orderable' => false, 'searchable' => false]);
+
+        return view('guest.index')->with(compact('html'));
+    }
 }
